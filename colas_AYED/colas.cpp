5d334fc4e69719ea783e2b7296423a73fcc8cc36
JoniMujica <57498116+JoniMujica@users.noreply.github.com>
2020-12-22 17:49:02 -0300

    termine el ejercicio02

--- colas_AYED/colas.cpp
+++ colas_AYED/colas.cpp
@@ -12,31 +12,22 @@ struct Nodo {
 };
 
 void queue(Nodo*&, Nodo*&, Registro);
-Registro unqueue(Nodo*&, Nodo*&);
-bool isDel(Nodo*&, Nodo*&);
-void MostrarElementos(Nodo*&);
+int cantidadElementos(Nodo*&);
 int main() {
 	Nodo* fte = NULL;
 	Nodo* fin = NULL;
-
-	//hago este for para rellenar los datos en la cola
 	Registro test;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 7; i++)
 	{
 		test.algo = i;
 		test.otro_algo = 2 * i;
 		queue(fte, fin, test);
 	}
-	 // MostrarElementos(fte);    //agregue esto para saber lo que tengo en la cola por primera ves antes de eliminar los 2 nodos
-	if (isDel(fte, fin))  //aca compruebo si se pudo eliminar
-		cout << "se pudo eliminar" << endl;
-	else
-		cout << "no es posible eliminar" << endl;
 
-	MostrarElementos(fte); //muestro los valores de la cola modificada
+	int c = cantidadElementos(fte);
+	cout << "cantidad de nodos: " << c << endl;
 	return 0;
 }
-
 void queue(Nodo*& fte, Nodo*& fin, Registro rgt) {
 	Nodo* aux = new Nodo();
 	aux->reg = rgt;
@@ -47,36 +38,13 @@ void queue(Nodo*& fte, Nodo*& fin, Registro rgt) {
 		fin->sig = aux;
 	fin = aux;
 }
-
-Registro unqueue(Nodo*& fte, Nodo*& fin) {
-	Registro x;
-	Nodo* p = fte;
-	x = p->reg;
-	fte = p->sig;
-	if (fte == NULL)
-		fin = NULL;
-	delete p;
-	return x;
-}
-
-bool isDel(Nodo*& fte, Nodo*& fin) {
-	for (int i = 0; i < 2; i++)
-	{
-		if (fte != NULL)
-			unqueue(fte, fin);
-		else
-			return false;
-	}
-	return true;
-}
-void MostrarElementos(Nodo*& fte) {
+int cantidadElementos(Nodo*& fte) {
 	Nodo* aux = fte;
-	cout << "========elementos========" << endl;
+	int cant = 0;
 	while (aux != NULL)
 	{
-		cout << "________elemento_____" << endl;
-		cout << "elementos de la pila " << aux->reg.algo << endl; 
-		cout << "elementos de la pila " << aux->reg.otro_algo << endl;
 		aux = aux->sig;
+		cant++;
 	}
+	return cant;
 }
